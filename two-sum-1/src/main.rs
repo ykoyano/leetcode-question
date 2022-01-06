f32dffc9611305b9d0d79f0a8e3dd7eb89cd0cf9
ykoyano <yuji.koyano.800@gmail.com>
2022-01-06 20:32:52 +0900

    Add answer

---
+++ two-sum-1/src/main.rs
@@ -0,0 +1,33 @@
+struct Solution {}
+
+impl Solution {
+    fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for (index, num) in nums.iter().enumerate() {
+            let residual = target - num;
+
+            let index2 = nums
+                .iter()
+                .position(|&r| r == residual);
+
+            let index3 = match index2 {
+                Some(index2) => index2,
+                None => continue
+            };
+
+            if index == index3 {
+                continue;
+            }
+
+            return vec![index as i32, index3 as i32];
+        }
+
+        return vec![];
+    }
+}
+
+fn main() {
+    let nums = vec![3, 2, 4];
+    let target = 6;
+    let answer = Solution::two_sum(nums, target);
+    println!("{:?}", answer);
+}
